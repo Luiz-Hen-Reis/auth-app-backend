98a8011664f5207aa65d0765b0b507569669d50a
Luiz Henrique <lhcontato2020@gmail.com>
2023-10-28 21:59:58 -0300

    feat: create users table migration

---
+++ src/db/migrations/20231029003559-users-table.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.DataTypes.INTEGER,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.DataTypes.STRING,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};

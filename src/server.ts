d0a03ba1c09199038b4c0ce0336c6c81f68722f0
Luiz Henrique <lhcontato2020@gmail.com>
2023-10-31 21:05:24 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { config } from "dotenv";
 import { sequelize } from "./db";
 import { router } from "../routes";
@@ -7,6 +8,7 @@ config();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 const SERVER_PORT = process.env.SERVER_PORT;
